cc9863de1485d81d402e6693382fd76823d0d1aa
JohnSmith110 <zc2690109215@qq.com>
2024-07-07 13:23:11 +0800

    first commit

---
+++ 1.py
@@ -0,0 +1,19 @@
+
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+print("选择运算：")
+print("1.相加")
+print("2.相减")
+print("3.相乘")
+print("4.相除")
